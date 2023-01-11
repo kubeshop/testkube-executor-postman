9540dd6b571056a0e92dde5ae27977f5c43da019
Lilla Vass <lilla@kubeshop.io>
2023-01-11 19:22:46 +0100

    fix: avoid panic

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	r, err := newman.NewNewmanRunner()
 	if err != nil {
-		log.Fatalf("%s could not run Postman tests: %w", ui.IconCross, err)
+		log.Fatalf("%s could not run Postman tests: %s", ui.IconCross, err.Error())
 	}
 	agent.Run(r, os.Args)
 }
